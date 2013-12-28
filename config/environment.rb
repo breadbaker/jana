9337a8307c28ba789421b5fe292b55c6b0edc469
App Academy Student <student@appacademy.io>
2013-12-28 11:21:33 -0500

    shit

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,10 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Jana::Application.initialize!
 
+
+
+
 ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.default_charset = "utf-8"
