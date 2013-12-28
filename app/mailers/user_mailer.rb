c6f393dad5434413c192f056168ece77b66c6309
App Academy Student <student@appacademy.io>
2013-12-27 19:21:25 -0500

    a

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default from: "welcome@janabaker.us"
-  
+  default from: "welcome@janas-art.herokuapp.com"
+
   def confirmation_email(user)
     @user = user
     @url = 'http://janabaker.us/confirm'
     mail( to: user.email, subject: 'Confirm Your Account')
   end
-  
+
 end
