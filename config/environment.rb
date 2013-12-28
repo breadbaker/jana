1b773fdf8e61d77dc8208f220f72a5c0d2b5f9d8
App Academy Student <student@appacademy.io>
2013-12-28 11:22:38 -0500

    def ca

--- config/environment.rb
+++ config/environment.rb
@@ -11,4 +11,3 @@ Jana::Application.initialize!
 ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.default_charset = "utf-8"
