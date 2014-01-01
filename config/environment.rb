c78bcfe47fca4b84b7f4037a819619cfdcec051a
App Academy Student <student@appacademy.io>
2014-01-01 16:15:38 -0500

    sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Jana::Application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'the-masonry.herokuapp.com',
+  :domain => 'janabaker.us',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
