5f615859ac9439e70a3f3e871f2a27333338492c
App Academy Student <student@appacademy.io>
2014-01-01 16:14:46 -0500

    sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -5,19 +5,28 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Jana::Application.initialize!
 
-
-
-
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.default_url_options = { :host => 'www.janabaker.us' }
-ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => "gmail.com",
-    :authentication => :login,
-    :user_name => ENV['GMAIL_USER'],
-    :password => ENV['GMAIL_PASSWORD']
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'the-masonry.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
 }
+#
+#
+#
+# ActionMailer::Base.perform_deliveries = true
+# ActionMailer::Base.raise_delivery_errors = true
+# ActionMailer::Base.default_url_options = { :host => 'www.janabaker.us' }
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#     :enable_starttls_auto => true,
+#     :address => "smtp.gmail.com",
+#     :port => 587,
+#     :domain => "gmail.com",
+#     :authentication => :login,
+#     :user_name => ENV['GMAIL_USER'],
+#     :password => ENV['GMAIL_PASSWORD']
+# }
